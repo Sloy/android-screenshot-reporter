7dcc11a9dd7ebe7480b25611740d04952a21a2a5
cristian-garcia <cristian.garcia@scmspain.com>
2019-12-12 19:01:11 +0100

    project.tasks.add(...) is forbiden, let's use the configuration avoidance API

--- plugin/src/main/kotlin/com/sloydev/screenshotreporter/gradle/ScreenshotReporterPlugin.kt
+++ plugin/src/main/kotlin/com/sloydev/screenshotreporter/gradle/ScreenshotReporterPlugin.kt
@@ -2,11 +2,9 @@ package com.sloydev.screenshotreporter.gradle
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.api.ApplicationVariant
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import kotlin.reflect.KClass
 
 open class ScreenshotReporterPlugin : Plugin<Project> {
 
@@ -41,53 +39,27 @@ open class ScreenshotReporterPlugin : Plugin<Project> {
             project.tasks.findByName(it)
         } ?: throw IllegalStateException("Task ${reporterExtension.screenshotsTask} not found")
 
-        val setupTask = project.createTask(
-                type = SetupScreenshotsTask::class,
-                name = SetupScreenshotsTask.TASK_NAME,
-                description = "Setup the device to allow storing screenshots and clear any previous ones",
-                dependsOn = listOf(variant.install)) // install first to let us grant permissions
-                .apply {
-                    appPackage = packageName
-                    sdkDirectory = appExtension.sdkDirectory
-                }
-        project.tasks.add(setupTask)
+        val setupTask = project.tasks.register(SetupScreenshotsTask.TASK_NAME, SetupScreenshotsTask::class.java) {
+            it.group = GROUP
+            it.description = "Setup the device to allow storing screenshots and clear any previous ones"
+            it.dependsOn(variant.installProvider)
+            it.appPackage = packageName
+            it.sdkDirectory = appExtension.sdkDirectory
+        }
 
-        val pullTask = project.createTask(
-                type = PullScreenshotsTask::class,
-                name = PullScreenshotsTask.TASK_NAME,
-                description = "Downloads screenshots from the device")
-                .apply {
-                    appPackage = packageName
-                    sdkDirectory = appExtension.sdkDirectory
-                }
-        project.tasks.add(pullTask)
+        val pullTask = project.tasks.register(PullScreenshotsTask.TASK_NAME, PullScreenshotsTask::class.java) {
+            it.group = GROUP
+            it.description = "Downloads screenshots from the device"
+            it.mustRunAfter(screenshotsTask)
+            it.appPackage = packageName
+            it.sdkDirectory = appExtension.sdkDirectory
+        }
 
-        val generateTask = project.createTask(
-                type = DefaultTask::class,
-                name = "generateScreenshots",
-                dependsOn = listOf(setupTask, screenshotsTask, pullTask)
-        )
-        project.tasks.add(generateTask)
-        pullTask.mustRunAfter(screenshotsTask)
+        project.tasks.register("generateScreenshots") {
+            it.group = GROUP
+            it.dependsOn(setupTask, screenshotsTask, pullTask)
+        }
+
         screenshotsTask.mustRunAfter(setupTask)
-    }
-
-    private fun <T : DefaultTask> Project.createTask(
-            type: KClass<T>,
-            name: String,
-            group: String = GROUP,
-            description: String? = null,
-            dependsOn: List<Task>? = null,
-            finalizedBy: List<Task>? = null
-    )
-            : T {
-        return type.java.cast(project.tasks.create(LinkedHashMap<String, Any>().apply {
-            put("name", name)
-            put("type", type.java)
-            put("group", group)
-            description?.let { put("description", it) }
-            dependsOn?.let { put("dependsOn", it) }
-            finalizedBy?.let { put("finalizedBy", it) }
-        }))
     }
 }
